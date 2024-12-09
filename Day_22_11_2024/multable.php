98d6373f76add90a4c9a3e9a5cadc7ebceb26b8f
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-09 11:41:44 +0000

    function seperated

--- Day_22_11_2024/multable.php
+++ Day_22_11_2024/multable.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'conf/dbconf.php';
+require_once 'myfunctions/func.php';
 
 
 
@@ -8,49 +9,7 @@ echo "<br>";
 showTable("teacher",$connect);
 
 
-function showTable1($tname,$connect,$colnames){
-    try {
-       $sql = "SELECT ";
-        for($i=0; $i<sizeof($colnames)-1; $i++){
-            $sql.=$colnames[$i].",";
-        }
-      $sql.=$colnames[sizeof($colnames)-1]." FROM $tname "; 
 
-      $result = mysqli_query($connect, $sql);
-     
-        
-            if (mysqli_num_rows($result) > 0) {
-           
-                echo "<table border='1' cellpadding='5' cellspacing='0'>";
-                echo "<tr>";
-                
-                $columns = mysqli_fetch_fields($result);
-                foreach ($columns as $column) {
-                   
-        
-                    echo "<th>$column->name</th>";
-                }
-                echo "</tr>";
-        
-                
-                while ($row = mysqli_fetch_assoc($result)) {
-                    
-                    echo "<tr>";
-                    foreach ($row as $value) {
-                        echo "<td>$value</td>";
-                    }
-                    echo "</tr>";
-                }      
-                echo "</table>";
-            } else {
-                echo "No result";
-            }
-          
-    }
-    catch (Exception $e) {
-        die($e->getMessage());
-    }
-    }
     echo "<br>";
     showTable1("teacher",$connect,["name","subject"]);
 ?>
