a311b10aac8f140f8dc3f226877f742f04c5c580
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-10 07:21:33 +0000

    listofname.php commited

---
+++ Day_06_12_2024/listofname.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Of Names</title>
+</head>
+<body>
+    <?php
+    require_once 'conf/dbconf.php';
+    require_once 'myfunctions/func.php';
+   
+   
+    getName($connect);
+
+?>
+
+ <?php
+ 
+
+ ?>  
+
+</body>
+</html>
