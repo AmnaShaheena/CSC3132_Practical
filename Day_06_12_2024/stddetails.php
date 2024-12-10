632ee19c40769cfa7be1d859501cd6e5c0bcba5d
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-10 07:19:21 +0000

    call function

--- Day_06_12_2024/stddetails.php
+++ Day_06_12_2024/stddetails.php
@@ -1,25 +1,20 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>printTable</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Students Details</title>
 </head>
 <body>
-   
-<?php
-
- 
-require_once '../conf/dbconf.php'; 
-require_once '../myfunctions/func.php';
-
-
- 
- $regno = $_GET['regno'];
-
-stdDetails($regno,$connect);
-
-
-?>
-
-
+    
+    <?php
+    require_once 'conf/dbconf.php';
+    require_once 'myfunctions/func.php';
+    $regno=$_GET['regno'];
+    getDetails($regno,$connect);
+    //echo "<h4>My name is ...</h4>";
+    ?>
+    
 </body>
 </html>
